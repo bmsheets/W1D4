d044817e3989ce8d1e76d1dda298d7eb01016b10
App Academy Student <student@appacademy.io>
2017-10-05 15:18:19 -0700

    Fix many bugs in board.rb, sudoku.rb

--- board.rb
+++ board.rb
@@ -1,7 +1,8 @@
 require_relative "tile"
-
+require 'byebug'
 class Board
   attr_reader :grid
+  attr_accessor :rows
 
   def self.empty_grid
     @grid = Array.new(9) do
@@ -11,28 +12,29 @@ class Board
   end
 
   def self.from_file(filename)
-    rows = File.readlines("filename").map(:chomp)
+    rows = File.readlines(filename).map(&:chomp)
     tiles = rows.map do |row|
-      nums = row.split("").map { |char| parseInt(char) }
-      nums.map { |num| Tle.new(num) }
+      nums = row.split("").map(&:to_i)
+      nums.map { |num| Tile.new(num) }
     end
 
     self.new(tiles)
   end
 
   def initialize(grid = self.empty_grid)
+    @rows = grid
     @grid = grid
   end
 
   def [](pos)
-    pos = x,y
+    x, y = pos
     grid[x][y]
   end
 
   def []=(pos, value)
     x, y = pos
     tile = grid[x][y]
-    tile.value = new_value
+    tile.value = value
   end
 
   def columns
@@ -40,7 +42,7 @@ class Board
   end
 
   def render
-    puts "(0..8).to_a.join(" ")"
+    puts "  #{(0..8).to_a.join(" ")}"
     grid.each_with_index do |row, i|
       puts "#{i} #{row.join(" ")}"
     end
@@ -51,9 +53,10 @@ class Board
     grid.size
   end
 
-  alias_method :rows, :size
+  #alias_method :rows, :size
 
   def solved?
+    #debugger
     rows.all? { |row| solved_set?(row) } &&
       columns.all? { |col| solved_set?(col) } &&
       squares.all? { |square| solved_set?(square) }
